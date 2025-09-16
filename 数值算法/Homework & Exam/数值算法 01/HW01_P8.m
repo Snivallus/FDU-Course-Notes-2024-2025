c648c7f699babb88798972cdc5089c192904fe4d
Snivellus Snape <21307140051@m.fudan.edu.cn>
2025-09-16 15:00:55 +0800

    update Homework 01

--- 数值算法/Homework & Exam/数值算法 01/HW01_P8.m
+++ 数值算法/Homework & Exam/数值算法 01/HW01_P8.m
@@ -1,21 +1,24 @@
+clear; clc; close all;
+
 % 生成测试数据
 rng(51);
-n = 1000;
-U = triu(rand(n)*sqrt(n)) + 2*sqrt(n)*eye(n); % 保证随机生成的上三角阵的条件数适中
-b = rand(n,1);
+n = 10000;
+U = triu(randn(n)) +  sqrt(n) * eye(n)...
+    + 1i * (triu(randn(n)) + sqrt(n) * eye(n)); % 保证随机生成的上三角阵的条件数适中
+b = randn(n,1) + 1i * randn(n,1);
 
-% 解法 1: Backward_Sweep_Raw
-x1 = Backward_Sweep_Raw(U,b);
+% 解法 1: Backward_Sweep_Row
+x1 = Backward_Sweep_Row(U, b);
 
-% 解法 2: Backward_Sweep
-x2 = Backward_Sweep(U,b);
+% 解法 2: Backward_Sweep_Column
+x2 = Backward_Sweep_Column(U, b);
 
 % 验证是否正确
-fprintf('||U*x1 - b||_inf = %.2e\n', norm(U*x1 - b, Inf));
-fprintf('||U*x2 - b||_inf = %.2e\n', norm(U*x2 - b, Inf));
+fprintf('||U*x1 - b||_inf = %.2e\n', norm(U*x1 - b, "inf"));
+fprintf('||U*x2 - b||_inf = %.2e\n', norm(U*x2 - b, "inf"));
 
 % ======== 方法 1 =========
-function b = Backward_Sweep_Raw(U,b)
+function b = Backward_Sweep_Row(U,b)
     n = length(b);
     b(n) = b(n)/U(n,n);
     for i = n-1:-1:1
@@ -24,7 +27,7 @@ function b = Backward_Sweep_Raw(U,b)
 end
 
 % ======== 方法 2 =========
-function b = Backward_Sweep(U,b)
+function b = Backward_Sweep_Column(U,b)
     n = length(b);
     for i = n:-1:2
         b(i) = b(i)/U(i,i);
